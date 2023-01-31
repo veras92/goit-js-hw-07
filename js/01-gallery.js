bcab2d0c5241b2c59c7f95ffa427e7903e37b37c
veras92 <110496079+veras92@users.noreply.github.com>
2023-01-31 21:56:50 +0100

    finished the task

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+
+renderGalleryItems(galleryItems);
+
+galleryList.addEventListener("click", onGalleryListClick);
+function onGalleryListClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source} " alt="${event.target.alt}">`
+  );
+  instance.show();
+}
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+function renderGalleryItems(galleryItems) {
+  galleryList.innerHTML = createGalleryItemsMarkup(galleryItems);
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,37 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+renderGalleryItems(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+galleryList.addEventListener("click", onGalleryListClick);
+function onGalleryListClick(event) {
+  event.preventDefault();
+}
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+function renderGalleryItems(galleryItems) {
+  galleryList.innerHTML = createGalleryItemsMarkup(galleryItems);
+}
+
+console.log(galleryItems);
